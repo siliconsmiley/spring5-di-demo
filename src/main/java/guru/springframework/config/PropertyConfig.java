12cab504866a30926d2f872ebae8f1f420fcdb83
Matthew H. Wagner <siliconsmiley@gmail.com>
2020-06-17 12:31:45 -0400

    Added external property source and FakeDataSource.java.

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
+/**
+ * mhw
+ */
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
